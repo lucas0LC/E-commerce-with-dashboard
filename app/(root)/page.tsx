8aa477c89d8a6b42b2e76f60ad96134a2b9d11b1
Lucas <lucascoreia123@gmail.com>
2024-04-10 17:21:17 -0300

    authentication (admin)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
+import { UserButton } from "@clerk/nextjs";
+
+/*export default function Home() {
   return <p>Hello Admin Dashboard</p>;
-}
+}*/
+
+const SetupPage = () => {
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default SetupPage;
